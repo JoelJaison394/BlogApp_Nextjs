62ae146c5f178a3420c92c67bdf2cf01e63ae355
Joel Jaison <joeljaison394@gmail.com>
2023-02-09 19:49:44 +0530

    sanitystudio setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <h1 className='text-4xl'>Welcome to the blog</h1>
+        <p>Some content</p>
+    </div>
+  )
+}
+
+export default page
