1e8b1d5a73f1161b0b0377e63c4ba8ec35a23f4a
Joel Jaison <joeljaison394@gmail.com>
2023-02-11 19:42:08 +0530

    sanity query added

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
 
-function HomePage() {
-  return (
-    <div>
-        <h1 className='text-4xl'>Welcome to the blog</h1>
-    </div>
-  )
+export const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+async function HomePage() {
+  if (previewData()) {
+    return;
+    <PreviewSuspense fallback={
+      <div role="status">
+        <p className="text-center text-lg animate-pulse text-[#f7aboa]">Loading Preview Data...</p>
+      </div>
+    }>
+      <PreviewBlogList query={query}/>
+    </PreviewSuspense>;
+  }
+
+  const posts = await client.fetch(query);
+  return 
+
+    <BlogList posts={posts}/>
+
 }
 
-export default HomePage
+export default HomePage;
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,18 @@
 
 function Banner() {
   return (
-    <div>
-        
+    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+        <div className="">
+            <h1 className="text-7xl">Developers Blog</h1>
+            <h2 className="mt-5 md:mt-2">
+                Welcome to{""}
+                <span className="underline decoration-4 decoration-[#f7ab0a]">Every Developers</span>{""}
+                favorite blog in the Devosphere
+            </h2>
+        </div>
+        <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
+            New Product features | the latest in the technology | The weekly debugging nightmares & More!
+        </p>
     </div>
   )
 }
